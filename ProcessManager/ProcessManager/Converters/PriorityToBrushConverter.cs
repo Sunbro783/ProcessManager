32b2b599946086c41907019274e338b8cfed02f6
kaves <kaves@MSI>
2026-02-27 16:49:22 +0300

    Добавлен конвертер PriorityToBrushConverter для цветовой индикации приоритетов

--- ProcessManager/ProcessManager/Converters/PriorityToBrushConverter.cs
+++ ProcessManager/ProcessManager/Converters/PriorityToBrushConverter.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace ProcessManager.Converters
 {
-    internal class PriorityToBrushConverter
+    public class PriorityToBrushConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            if (value is ProcessPriorityClass p)
+            {
+                if (p == ProcessPriorityClass.High)
+                    return Brushes.Orange;
+
+                if (p == ProcessPriorityClass.RealTime)
+                    return Brushes.Red;
+            }
+
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing; // ОБЯЗАТЕЛЬНО
+        }
     }
 }

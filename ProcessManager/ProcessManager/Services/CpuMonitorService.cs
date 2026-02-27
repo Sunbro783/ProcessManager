7de41965344b560b287952905a6d537f772fdbcd
kaves <kaves@MSI>
2026-02-27 16:31:43 +0300

    Реализован CpuMonitorService для мониторинга загрузки CPU по ядрам

--- ProcessManager/ProcessManager/Services/CpuMonitorService.cs
+++ ProcessManager/ProcessManager/Services/CpuMonitorService.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace ProcessManager.Services
 {
-    internal class CpuMonitorService
+    public class CpuMonitorService
     {
+        private PerformanceCounter[] _counters;
+
+        public CpuMonitorService()
+        {
+            int cores = System.Environment.ProcessorCount;
+            _counters = new PerformanceCounter[cores];
+
+            for (int i = 0; i < cores; i++)
+            {
+                _counters[i] = new PerformanceCounter(
+                    "Processor", "% Processor Time", i.ToString());
+                _counters[i].NextValue();
+            }
+        }
+
+        public float[] GetCpuUsage()
+        {
+            float[] values = new float[_counters.Length];
+            for (int i = 0; i < _counters.Length; i++)
+                values[i] = _counters[i].NextValue();
+            return values;
+        }
     }
 }

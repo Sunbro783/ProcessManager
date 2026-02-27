92040b8892bcb132f80c67837b0cd8a3d33b5c39
kaves <kaves@MSI>
2026-02-27 16:41:21 +0300

    Реализован RelayCommand для поддержки команд в MVVM

--- ProcessManager/ProcessManager/VievModels/RelayCommand.cs
+++ ProcessManager/ProcessManager/VievModels/RelayCommand.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace ProcessManager.VievModels
+namespace ProcessManager.ViewModels
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute,
+            Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+            => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter)
+            => _execute(parameter);
+
+        public void RaiseCanExecuteChanged()
+            => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }

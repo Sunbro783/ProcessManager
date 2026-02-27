4afdd15a64b0e6ebdd314cc9c2cc3d4bc97bc64e
kaves <kaves@MSI>
2026-02-27 16:46:29 +0300

    Реализован ProcessInfo для представления данных о процессе

--- ProcessManager/ProcessManager/Models/ProcessInfo.cs
+++ ProcessManager/ProcessManager/Models/ProcessInfo.cs
@@ -1,12 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace ProcessManager.Models
 {
-    internal class ProcessInfo
+    public class ProcessInfo : INotifyPropertyChanged
     {
+        private ProcessPriorityClass _priority;
+        private double _memoryMB;
+        private TimeSpan _cpuTime;
+        private int _threadCount;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+        public bool HasWindow { get; set; }
+        public long? AffinityMask { get; set; }
+
+        public ProcessPriorityClass Priority
+        {
+            get => _priority;
+            set
+            {
+                if (_priority != value)
+                {
+                    _priority = value;
+                    OnPropertyChanged(nameof(Priority));
+                }
+            }
+        }
+
+        public double MemoryMB
+        {
+            get => _memoryMB;
+            set
+            {
+                if (_memoryMB != value)
+                {
+                    _memoryMB = value;
+                    OnPropertyChanged(nameof(MemoryMB));
+                }
+            }
+        }
+
+        public TimeSpan CpuTime
+        {
+            get => _cpuTime;
+            set
+            {
+                if (_cpuTime != value)
+                {
+                    _cpuTime = value;
+                    OnPropertyChanged(nameof(CpuTime));
+                }
+            }
+        }
+
+        public int ThreadCount
+        {
+            get => _threadCount;
+            set
+            {
+                if (_threadCount != value)
+                {
+                    _threadCount = value;
+                    OnPropertyChanged(nameof(ThreadCount));
+                }
+            }
+        }
+
+        // 🔥 Метод обновления без пересоздания объекта
+        public void UpdateFrom(ProcessInfo other)
+        {
+            Priority = other.Priority;
+            MemoryMB = other.MemoryMB;
+            CpuTime = other.CpuTime;
+            ThreadCount = other.ThreadCount;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
